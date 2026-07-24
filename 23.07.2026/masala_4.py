97399bc42e2fc5e36148b043c1e005ec477edbd3
maryamakhm44 <maryamakhm44@gmail.com>
2026-07-24 15:15:32 +0300

    4-masala bajarildi, tartibli bo'lishi uchun javob jadval ko'rinishida

---
+++ 23.07.2026/masala_4.py
@@ -0,0 +1,21 @@
+def search_by_genre(cinema,janr)->list:
+	res = []
+	for x in cinema:
+		if x['genre'] == janr:
+			res.append(x)
+	return res
+
+if __name__ == "__main__":
+	cinema = [{"title": "Avatar", "genre": "Fantastika", "price": 40000},
+	{"title": "Sherlock", "genre": "Detektiv", "price": 30000},
+	{"title": "Oq yo'l", "genre": "Drama", "price": 25000},
+	{"title": "Dune", "genre": "Fantastika", "price": 35000},
+	{"title": "Interstellar", "genre": "Fantastika", "price": 45000},
+	{"title": "Titanic", "genre": "Drama", "price": 28000},
+	{"title": "Inception", "genre": "Fantastika", "price": 38000},
+	{"title": "The Batman", "genre": "Detektiv", "price": 42000}]
+
+	janr = input("Izlayotgan filmingiz janri: ")
+	result = search_by_genre(cinema,janr)
+	for x in result:
+		print(f"{x['title']:14s} | {x['genre']:10s} | {x['price']:6d} so'm")
