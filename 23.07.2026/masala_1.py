5fbd4df91220457866e88583b3d777976ce822f2
maryamakhm44 <maryamakhm44@gmail.com>
2026-07-24 14:21:09 +0300

    1-masala yakunlandi

--- 23.07.2026/masala_1.py
+++ 23.07.2026/masala_1.py
@@ -1,14 +1,18 @@
 def find_top_three_nums(d):
+	res = []
 	for x in d:
-		if len(x) > 3:
-	 		x = sorted(x)
-			for i in range(x - 3):
-				x.remove(x[i])
+		if len(x) <= 3:
+			x = tuple(sorted(x,reverse = True))
 		else:
-			x = tuple(sorted(x))
-		print(x)
+			mx = []
+			x = list(x)
+			for _ in range(3):
+				mx.append(max(x))
+				x.remove(max(x))
+			x = tuple(mx)
+		res.append(x)
+	return res
 
 if __name__ == "__main__":
 	data = [(10,20,30,11),(5,15),(40,),(7,8,50,3)]
-	#print(find_top_three_nums(data))
-	find_top_three_nums(data)
+	print(find_top_three_nums(data))
