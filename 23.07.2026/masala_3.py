b0333e38395f0a550b9e012d5763ea2b3e015578
maryamakhm44 <maryamakhm44@gmail.com>
2026-07-24 15:32:33 +0300

    3-masala yakunlandi

--- 23.07.2026/masala_3.py
+++ 23.07.2026/masala_3.py
@@ -5,11 +5,16 @@ def find_nearest_vowel(s)->str:
 		return "Faqat bitta kichik harf bo'lishi kerak"
 	else:
 		if ord(s) <= 122 and ord(s) >= 97:
-			res =
+			dc = {97: "a", 101: "e", 105: "i", 111: "o", 117: "u"}
+			min = None
+			for x in dc:
+				n = x - ord(s)
+				if min is None or min > abs(n):
+					min = abs(n)
+					res = dc[x]
 			return res
 		else:
 			return "Faqat bitta kichik harf bo'lishi kerak"
-
 
 if __name__ == "__main__":
 	l = input("Kichik harf kiriting: ")
