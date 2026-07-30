e2098f8d8879bcce2b6a8de5a5c3046253c4cd74
maryamakhm44 <maryamakhm44@gmail.com>
2026-07-30 12:23:56 +0300

    1-masala ham bajarildi, funksiyalari to'g'ri ishlavotti

--- 28.07.2026/masala_1.py
+++ 28.07.2026/masala_1.py
@@ -1,6 +1,6 @@
 import os
 class account:
-	def __init__(self,id,name,b):
+	def __init__(self,id,name,b = 0):
 		self.id = id
 		self.name = name
 		self.balance = b
@@ -14,36 +14,62 @@ class account:
 	def get_balance(self):
 		return self.balance
 
-	def show_all_acc_info(self,ls):
-		for x in range(len(ls)):
-			print(f"\t|{self.get_id():3s}|{self.get_name():20s}|{self.get_balance():15.2f}|")
-			print(f"\t|-----------------------------------|")
+	def pul_sol(self,amount):
+		self.balance += amount
+		return self.balance
 
-#	def pul_sol(amount):
+	def pul_chiqar(self,amount):
+		if self.balance >= amount:
+			self.balance -= amount
+		else:
+			print("Balans yetarli emas!")
+		return self.balance
 
-
-#	def pul_chiqar(amount):
-
-
-#	def pul_otkaz(another,amount):
-
-
+	def pul_otkaz(self,another,amount):
+		if self.balance >= amount:
+			self.balance -= amount
+			another.balance += amount
+		else:
+			print("Balans yetarli emas!")
+		return self.balance
 
 
 if __name__ == "__main__":
 	os.system("clear")
-	accounts = []
-	n = int(input("Nechta akkaunt kiritmoqchisiz: "))
-	for x in range(n):
-		id = input("\nAkkaunt id: ")
-		akk = input("Akkaunt nomi: ")
-		balans = int(input("Balans: "))
-		ac = account(id,akk,balans)
-		accounts.append(ac)
-		print("Account added\n")
 
+	id1 = input("Akkaunt id: ")
+	akk1 = input("Akkaunt nomi: ")
+	balans1 = int(input("Balans: "))
+	acc1 = account(id1,akk1,balans1)
+	print("Account1 added")
 
-	for i in range(len(accounts)):
-		print(accounts[i].get_name())
+	id2 = input("\n\nAkkaunt id: ")
+	akk2 = input("Akkaunt nomi: ")
+	balans2 = int(input("Balans: "))
+	acc2 = account(id2,akk2,balans2)
+	print("Account2 added")
 
-	ac.show_all_acc_info(accounts)
+	os.system("clear")
+
+	print("--- Account 1 ---")
+	print(f"ID:       {acc1.get_id()}")
+	print(f"Name:     {acc1.get_name()}")
+	print(f"Balans:   {acc1.get_balance()}")
+
+	print("\n\n--- Account 2 ---")
+	print(f"ID:       {acc2.get_id()}")
+	print(f"Name:     {acc2.get_name()}")
+	print(f"Balans:   {acc2.get_balance()}")
+
+	sol = int(input("\n\nQo'shiladigan summa: "))
+	chiq = int(input("Yechiladigan summa: "))
+
+	print(f"\nYangi balans (acc1):  {acc1.pul_sol(sol)}")
+	print(f"Yangi balans (acc2):  {acc2.pul_chiqar(chiq)}")
+
+	amount = int(input("\n2-accountga o'tkaziladigan summa: "))
+	acc1.pul_otkaz(acc2,amount)
+
+	print("\n\n--- Total ---")
+	print(f"1-account balansi:  {acc1.get_balance()}")
+	print(f"2-account balansi:  {acc2.get_balance()}")
