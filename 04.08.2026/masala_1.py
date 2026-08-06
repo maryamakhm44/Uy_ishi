22221e4ecd997a8a329f97151faa49b422433e6f
maryamakhm44 <maryamakhm44@gmail.com>
2026-08-06 08:37:53 +0300

    1-masala bajarildi

--- 04.08.2026/masala_1.py
+++ 04.08.2026/masala_1.py
@@ -1,1 +1,45 @@
-class employee(ABS):
+from abc import ABC,abstractmethod
+class Employee(ABC):
+	def __init__(self,name,salary):
+		self.name = name
+		self.salary = salary
+
+	@abstractmethod
+	def get_details(self):
+		pass
+
+	@abstractmethod
+	def calculate_bonus(self):
+		pass
+
+class Manager(Employee):
+	def __init__(self,name,salary,department):
+		super().__init__(name,salary)
+		self.department = department
+
+	def get_details(self):
+		print(f"""\nManager:     {self.name}\nSalary:      ${self.salary}\nDepartment:  {self.department}""")
+
+	def calculate_bonus(self):
+		return self.salary * 0.1
+
+class Developer(Employee):
+	def __init__(self,name,salary,pl):
+		super().__init__(name,salary)
+		self.programming_language = pl
+
+	def get_details(self):
+		print(f"""\nDeveloper:   {self.name}\nSalary:      ${self.salary}\nProgramming language: {self.programming_language}""")
+
+	def calculate_bonus(self):
+		return self.salary * 0.05
+
+if __name__ == "__main__":
+	manager = Manager("Alice",1200,"Sales")
+	developer = Developer("Bob",1000,"Python")
+
+	manager.get_details()
+	developer.get_details()
+
+	print(f"\nBonus (manager):     ${manager.calculate_bonus()}")
+	print(f"Bonus (developer):   ${developer.calculate_bonus()}")
